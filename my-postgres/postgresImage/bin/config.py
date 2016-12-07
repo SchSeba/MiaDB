a696efcb0bdf731bfd32dee4df3a003209c2595c
sebassch@gmail.com <JawbreaKer1993>
2016-12-07 17:24:56 +0200

    bug fix (multi start service cause split brain)

--- my-postgres/postgresImage/bin/config.py
+++ my-postgres/postgresImage/bin/config.py
@@ -3,50 +3,65 @@ import subprocess
 
 
 def StartPostGresServer():
-    PGDATA = os.getenv("PGDATA")
-    print ("PGDATA = " + PGDATA)
     ChangePriv()
-    if os.path.isfile(os.path.join(PGDATA,"postmaster.pid")):
-        print ("Restart Server")
-        RestartMember(PGDATA)
+    if os.path.isfile(os.path.join(os.getenv("PGDATA"),"postmaster.pid")):
+        print ("Files are exits in " + os.getenv("PGDATA") + " Restart Server")
+        RestartMember()
     else:
-        print("Start FirstCreation")
-        FirstCreation(PGDATA)
+        print("First time running Start creating database")
+        FirstCreation()
 
 
 def ChangePriv():
+    # Postgres Data
     RunCommand("chown -R postgres $PGDATA")
     RunCommand("chmod -R 0700 $PGDATA")
-    RunCommand("chown -R postgres /var/lib/postgresql/archive/")
-    RunCommand("chmod -R 0700 /var/lib/postgresql/archive/")
 
+    # Archive
+    RunCommand("chown -R postgres:postgres /var/lib/postgresql/archive")
+    RunCommand("chmod -R 0700 /var/lib/postgresql/archive")
+
+    # Master File
     RunCommand("chown postgres /master")
     RunCommand("chmod 0700 /master")
 
-
-def FirstCreation(PGDATA):
+def RunRepConfigFile():
+    print ("Run RepMgr_Config.sh with the follow Args")
+    print ("CLUSTER_NODE_NETWORK_NAME= " + os.getenv("CLUSTER_NODE_NETWORK_NAME") +
+           "\n NODE_ID= " + os.getenv("NODE_ID") +
+           "\n NODE_NAME= " + os.getenv("NODE_NAME") +
+           "\n CLUSTER_NAME= " + os.getenv("CLUSTER_NAME"))
     RunCommand("/usr/local/bin/cluster/repmgr_config.sh")
+    print ("Finnish running repmgr_config.sh")
+
+
+def FirstCreation():
+    RunRepConfigFile()
 
     print ("Node Type " + os.getenv("INITIAL_NODE_TYPE"))
-
     if os.getenv("INITIAL_NODE_TYPE") == "master":
-        print ("Start Master INITIAL")
-        RunCommand("echo " + os.getenv("CLUSTER_NODE_NETWORK_NAME") + "> /master")
-        RunCommand("cp -f /usr/local/bin/cluster/primary.entrypoint.sh /docker-entrypoint-initdb.d/")
-        RunCommand("/docker-entrypoint.sh \"postgres\"")
-        #p = subprocess.Popen("/docker-entrypoint.sh \"postgres\" &", stdout=subprocess.PIPE, shell=True)
-        p = subprocess.Popen("exec gosu postgres \"postgres\"", stdout=subprocess.PIPE, shell=True)
+
+            print ("Start Master INITIAL First Time")
+            RunCommand("echo " + os.getenv("CLUSTER_NODE_NETWORK_NAME") + "> /master")
+            RunCommand("cp -f /usr/local/bin/cluster/primary.entrypoint.sh /docker-entrypoint-initdb.d/")
+            RunCommand("/docker-entrypoint.sh \"postgres\"")
+            p = subprocess.Popen("exec gosu postgres \"postgres\"", stdout=subprocess.PIPE, shell=True)
 
     else:
-        #p = subprocess.Popen("/usr/local/bin/cluster/standby.entrypoint.sh \"postgres\" &", stdout=subprocess.PIPE, shell=True)
-        print ("Delete files in folder: " + PGDATA)
-        RunCommand("rm -rf " + PGDATA + "/*")
+        print ("Delete files in folder: " + os.getenv("PGDATA"))
+        RunCommand("rm -rf " + os.getenv("PGDATA") + "/*")
+
         print ("INITIAL_NODE_TYPE Env: " + os.getenv("INITIAL_NODE_TYPE"))
         print ("REPLICATION_PRIMARY_HOST Env: " + os.getenv("REPLICATION_PRIMARY_HOST"))
-        p = subprocess.Popen("/usr/local/bin/cluster/standby.entrypoint.sh \"postgres\" &", stdout=subprocess.PIPE,
+        print ("Run Wait_DB, Waiting for Master to go up")
+        RunCommand(
+            "wait_db $REPLICATION_PRIMARY_HOST $REPLICATION_PRIMARY_PORT $REPLICATION_USER $REPLICATION_PASSWORD $REPLICATION_DB")
+        p = subprocess.Popen("/usr/local/bin/cluster/standby.entrypoint.sh \"postgres\"", stdout=subprocess.PIPE,
                          shell=True,env=os.environ)
 
-    print ("Start WaitDB")
+
+    print ("Run Wait_DB, Waiting for this dataBase")
+    print ("CLUSTER_NODE_NETWORK_NAME= " + os.getenv("CLUSTER_NODE_NETWORK_NAME"))
     RunCommand("wait_db $CLUSTER_NODE_NETWORK_NAME $REPLICATION_PRIMARY_PORT $REPLICATION_USER $REPLICATION_PASSWORD $REPLICATION_DB")
     print ("End WaitDB")
 
@@ -59,20 +74,42 @@ def FirstCreation(PGDATA):
     RunCommand("gosu postgres repmgrd -vvv --pid-file=/tmp/repmgrd.pid")
 
     #RunCommand("echo FirstCreation > $PGDATA/status")
+    # print ("communicate")
+    # (output, err) = p.communicate()
+    # print ("wait")
+    # p_status = p.wait()
 
-    print ("communicate")
-    (output, err) = p.communicate()
-    print ("wait")
-    p_status = p.wait()
+def RunDataBase():
+    print ("Start Running database")
+    p = subprocess.Popen("exec gosu postgres \"postgres\"", stdout=subprocess.PIPE, shell=True)
 
-def RestartMember(PGDATA):
-    #DOTO: Need to work here understand the cluster picture
-    os.environ["INITIAL_NODE_TYPE"] = "standby"
-    os.environ["REPLICATION_PRIMARY_HOST"] = RunCommand("cat /master")
-    # RunCommand("tempPrimaryHost=`cat /master`")
-    # RunCommand("REPLICATION_PRIMARY_HOST=$tempPrimaryHost")
-    FirstCreation(PGDATA)
-    print("Exit for now")
+    print ("Start WaitDB")
+    RunCommand(
+        "wait_db $CLUSTER_NODE_NETWORK_NAME $REPLICATION_PRIMARY_PORT $REPLICATION_USER $REPLICATION_PASSWORD $REPLICATION_DB")
+    print ("End WaitDB")
+
+    print ("Register to Repmgr as " + os.getenv("INITIAL_NODE_TYPE"))
+    RunCommand("gosu postgres repmgr $INITIAL_NODE_TYPE register --force")
+    RunCommand("gosu postgres repmgr cluster show")
+    RunCommand("rm -rf /tmp/repmgrd.pid")
+
+    print ("Start Repmgr")
+    RunCommand("gosu postgres repmgrd -vvv --pid-file=/tmp/repmgrd.pid")
+
+
+def RestartMember():
+    RunRepConfigFile()
+
+    if RunCommand("cat /master").__contains__(os.environ["CLUSTER_NODE_NETWORK_NAME"]):
+        print ("This was the master on shutdown, Start the server like master")
+        os.environ["INITIAL_NODE_TYPE"] = "master"
+        RunDataBase()
+
+    else:
+        os.environ["INITIAL_NODE_TYPE"] = "standby"
+        os.environ["REPLICATION_PRIMARY_HOST"] = RunCommand("cat /master")
+
+        FirstCreation()
 
 
 def RunCommand(command,env=os.environ):
--- my-postgres/postgresImage/bin/standby.entrypoint.sh
+++ my-postgres/postgresImage/bin/standby.entrypoint.sh
@@ -9,11 +9,11 @@ echo ">>> Starting standby node..."
 
 chmod g+s /run/postgresql
 chown -R postgres /run/postgresql
+
 echo replication command
 echo PGPASSWORD=$REPLICATION_PASSWORD gosu postgres repmgr -h $REPLICATION_PRIMARY_HOST -U $REPLICATION_USER -d $REPLICATION_DB -D $PGDATA standby clone -F
 PGPASSWORD=$REPLICATION_PASSWORD gosu postgres repmgr -h $REPLICATION_PRIMARY_HOST -U $REPLICATION_USER -d $REPLICATION_DB -D $PGDATA standby clone -F
 echo End replication
-
 
 
 if [ "${1:0:1}" = '-' ]; then
